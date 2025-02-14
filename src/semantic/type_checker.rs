d5edcb9e87261120bef9d40f33892a7b24f62239
y_mc <momo.toure27@gmail.com>
2025-02-14 10:34:33 +0100

    type checker

---
+++ src/semantic/type_checker.rs
@@ -0,0 +1,25 @@
+// use crate::parser::ast::{Expression, Type};
+//
+// pub struct TypeChecker {
+//     //Environnement de typage
+//     type_env: TypeEnv,
+//     // context de trait
+//     trait_context: TraitContext,
+//
+//
+//     // // Le contexte de typage
+//     // type_context: TypeContext,
+//     // // Le contexte de portée
+//     // scope_context: ScopeContext,
+//     // // Le contexte des emprunts
+//     // borrow_checker: BorrowChecker,
+// }
+//
+//
+//
+//
+//
+// impl TypeChecker {
+//     fn check_assignment(&mut self, target: &Expression, value: &Expression) -> Result<Type, TypeError>;
+//     fn check_method_call(&mut self, object: &Expr, method: &str) -> Result<Type, TypeError>;
+// }
