1b1fcf66ca04ad1bfa11c41ca4b16934c325aec4
y_mc <momo.toure27@gmail.com>
2025-05-08 23:59:54 +0200

    implementation de semantic

--- src/semantic/mod.rs
+++ src/semantic/mod.rs
@@ -2,6 +2,7 @@ pub mod analyser;
 pub mod semantic_error;
 pub mod ast_semantic;
 pub mod type_checker;
-pub mod borrow_check;
+pub mod borrow_checker;
 pub mod typerror;
 pub mod symbols;
+pub mod symbol_table;
