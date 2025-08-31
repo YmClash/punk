f2fae7e8a012d4a1fbf85d9630c06e2ce64e8a35
y_mc <momo.toure27@gmail.com>
2025-08-31 11:41:47 +0200

    mod update

--- src/semantic/flow/mod.rs
+++ src/semantic/flow/mod.rs
@@ -1,3 +1,7 @@
-// pub mod cfg;
-// pub mod analyser;
-// pub mod detector;
+pub mod cfg;
+pub mod analyser;
+pub mod detector;
+pub mod control_flow_graph;
+
+// Re-export des types importants
+pub use control_flow_graph::{BlockId, BasicBlock, ControlFlowGraph};
